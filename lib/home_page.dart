383ecba6b047226fa202d91dd3d50af845798670
Mustafa <rawuchiha15@gmail.com>
2022-05-30 14:27:27 +0530

    .

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,58 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:work_app/login_page.dart';
-import 'package:work_app/main.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
+    return Container(
       child: Scaffold(
         appBar: AppBar(
-          title: Text("Header"),
+          title: Text("My Work"),
         ),
         body: Image.asset(
           "assets/images/tree.jpg",
           fit: BoxFit.cover,
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text(
-            "Logout",
-            style: TextStyle(
-              color: Colors.black,
-              backgroundColor: Colors.red,
-            ),
-          ),
-        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {},
+        //   child: Text(
+        //     "Logout",
+        //     style: TextStyle(
+        //       color: Colors.black,
+        //       backgroundColor: Colors.red,
+        //     ),
+        //   ),
+        // ),
         drawer: Drawer(
-          child: ListView(
-            children: [
-              UserAccountsDrawerHeader(
-                accountName: Text('Oflutter.com'),
-                accountEmail: Text('example@gmail.com'),
-                currentAccountPicture: CircleAvatar(
-                  child: ClipOval(
-                    child: Image.network(
-                      'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
-                      fit: BoxFit.cover,
-                      width: 90,
-                      height: 90,
-                    ),
+            child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Oflutter.com'),
+              accountEmail: Text('example@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(
+                  child: Image.network(
+                    'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
+                    fit: BoxFit.cover,
+                    width: 90,
+                    height: 90,
                   ),
                 ),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(
-                          'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg')),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg'),
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+          ],
+        )),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:work_app/home_page.dart';
 import 'package:work_app/login_page.dart';
+import 'package:work_app/register.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,13 +14,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-      // initialRoute: "/home",
-      // routes: {
-      //   "/": (context) => LoginPage(),
-      //   "/home": (context) => HomePage(),
-      //   "/login": (context) => HomePage(),
-      // },
+      initialRoute: 'home',
+      routes: {
+        'login': (context) => MyLogin(),
+        'register': (context) => MyRegister(),
+        'home': (context) => HomePage(),
+      },
     );
   }
 }
